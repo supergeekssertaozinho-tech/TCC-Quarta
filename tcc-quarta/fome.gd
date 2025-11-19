fa478da1c71e2a8682f513d3a4f0d3f0fb61ba8b
SGSertaozinho02 <supergeekssertaozinho+aluno02@gmail.com>
2025-11-19 20:07:12 -0300

    Player e Fome interação

--- tcc-quarta/fome.gd
+++ tcc-quarta/fome.gd
@@ -1,17 +1,27 @@
 extends Control
 
-@onready var bar = $ColorRect          # Caminho para o ColorRect
-@export var decrease_rate: float = 50.0  # Pixels por segundo
+signal hunger_zero
+
+@onready var bar = $ColorRect
+@export var decrease_rate: float = 50.0
 
 var original_width: float
 
 func _ready() -> void:
-	original_width = bar.size.x        # Salva o tamanho inicial
+	original_width = bar.size.x
 
 func _process(delta: float) -> void:
-	# Diminui a largura com o tempo
+	# Diminui a fome
 	bar.size.x -= decrease_rate * delta
 
-	# Garante que não fique negativa
-	if bar.size.x < 0:
+	# DEBUG → mostrar no console sempre
+	print("HUNGER VALUE: ", bar.size.x)
+
+	# Impedir valor negativo
+	if bar.size.x <= 0:
 		bar.size.x = 0
+		emit_signal("hunger_zero")  # sinal de fome zerada
+
+
+func get_hunger_value() -> float:
+	return bar.size.x
